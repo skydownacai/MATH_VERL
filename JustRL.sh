4dd4a649a43d3396d22013b0eca3fa200722ec76
nlx0021 <nlxliwenye@126.com>
2026-03-28 14:11:12 +0000

    read_rollouts: support json_path arg

--- JustRL.sh
+++ JustRL.sh
@@ -1,14 +1,15 @@
 set -x
 
 export HF_ENDPOINT="https://hf-mirror.com"
-export YOUR_PROJECT_NAME="JustRL-grpo"
-export YOUR_RUN_NAME="JustRL-grpo-$(date +%Y%m%d-%H%M%S)"
+export YOUR_PROJECT_NAME="MATH_VERL"
+# export YOUR_RUN_NAME="JustRL-grpo-trainset-dapo-$(date +%Y%m%d-%H%M%S)"
+export YOUR_RUN_NAME="JustRL-grpo-trainset-dapo"
 
 # export NVTE_DEBUG=1 NVTE_DEBUG_LEVEL=2
 
 export CUDA_DEVICE_MAX_CONNECTIONS=1 # For megatron communication/computation overlapping
 
-train_path=../datasets/DeepScaleR-Preview/train.parquet
+train_path=../datasets/retool_dapo/train.parquet
 test_path=../datasets/retool_aime2024/train.parquet
 rollout_data_root=./outputs/rollout_data
 validation_data_root=./outputs/validate_data
@@ -57,8 +58,9 @@ PYTHONUNBUFFERED=1 python3 -m verl.trainer.main_ppo --config-path=config \
  trainer.rollout_data_dir="$rollout_data_dir" \
  +trainer.validation_data_dir="$validation_data_dir" \
  trainer.val_before_train=True \
+ trainer.log_val_generations=64 \
  trainer.n_gpus_per_node=2 \
  trainer.nnodes=1 \
- trainer.save_freq=50 \
+ trainer.save_freq=200 \
  trainer.test_freq=10 \
  trainer.total_epochs=3 2>&1 | tee $YOUR_PROJECT_NAME.log \
--- scripts/read_rollouts.py
+++ scripts/read_rollouts.py
@@ -6,11 +6,15 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--rollout_path", type=str, default="/data/personal/MATH_VERL/outputs/rollout_data", help="Path to the rollout data directory.")
 parser.add_argument("--project_name", type=str, help="Name of the project to read rollouts from.", default="DeepScaleR-grpo")
 parser.add_argument("--run_name", type=str, help="Name of the run to read rollouts from.", default="DeepScaleR-grpo-20260305-082905")
+parser.add_argument("--json_path", type=str, help="Path to the JSON file containing the rollout data.", default=None)
 parser.add_argument("--step", type=int, help="Step number of the rollout to read.", default=1)
 parser.add_argument("--index", type=int, help="Index of the trajectory to read.", default=0)
 args = parser.parse_args()
 
-json_path = os.path.join(args.rollout_path, args.project_name, args.run_name, f"{args.step}.jsonl")
+if not args.json_path:
+    json_path = os.path.join(args.rollout_path, args.project_name, args.run_name, f"{args.step}.jsonl")
+else:
+    json_path = args.json_path
 
 # Read the jsonl file. 
 with open(json_path, 'r', encoding='utf-8') as f:
